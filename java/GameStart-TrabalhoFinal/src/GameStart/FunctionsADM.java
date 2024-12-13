2c818632a8aa2063fc09e112bc50f2bff51e9a05
DuarteOliveira88 <oliveiraduarte4802@gmail.com>
2024-12-13 15:02:58 +0000

    s

--- java/GameStart-TrabalhoFinal/src/GameStart/FunctionsADM.java
+++ java/GameStart-TrabalhoFinal/src/GameStart/FunctionsADM.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static GameStart.FunctionsClient.jogosSemDup;
+import static GameStart.FunctionsClient.quantosJogosSemDup;
 import static GameStart.Menus.filesMenu;
 import static GameStart.PathFile.*;
 
@@ -52,11 +54,46 @@ public class FunctionsADM {
             option= input.next();
         }while (!(option.equals("1")||option.equals("2")||option.equals("3")||option.toLowerCase().trim().equals("sair")));
         if(!option.equals("sair")) {
-            Scanner in = new Scanner(new File(pathMenuFilesNum(Integer.parseInt(option)+1)));
-            while (in.hasNextLine()) {
-                String line = in.nextLine();
-                System.out.println(line);
+            switch (String.valueOf(Integer.parseInt(option)+1)){
+                case "2":
+                    ficheiroCategorias(option);
+                    break;
+                case "3":
+                    ficheiroClientes(option);
+                    break;
+                case "4":
+                    ficheiroVendas(option);
+                    break;
+                default:
+                    System.out.println("Deu erro");
             }
+        }
+    }
+    public static void ficheiroCategorias(String option) throws FileNotFoundException {
+        Scanner in = new Scanner(new File(pathMenuFilesNum(Integer.parseInt(option)+1)));
+        System.out.printf("%-15s   %-10s%n", "Nome Categoria", "Percentagem");
+        in.nextLine();
+        while (in.hasNextLine()) {
+            String[] cont =in.nextLine().split(";");
+            System.out.printf("%-15s | %-10s%n", cont[0], cont[1]);
+        }
+    }
+    public static void ficheiroClientes(String option) throws FileNotFoundException {
+        Scanner in = new Scanner(new File(pathMenuFilesNum(Integer.parseInt(option)+1)));
+        in.nextLine();
+        System.out.printf("%-4s   %-22s   %-15s   %-30s%n", "ID", "Nome", "Telemóvel", "Email");
+        while (in.hasNextLine()) {
+            String[] cont =in.nextLine().split(";");
+            System.out.printf("%-4s | %-22s | %-15s | %-30s%n", cont[0], cont[1], cont[2], cont[3]);
+        }
+    }
+    public static void ficheiroVendas(String option) throws FileNotFoundException {
+        Scanner in = new Scanner(new File(pathMenuFilesNum(Integer.parseInt(option)+1)));
+        in.nextLine();
+        System.out.printf("%-10s  %-10s    %-21s   %-15s   %-32s   %-10s%n", "ID Venda", "ID Cliente", "Editora", "Categoria", "Jogo", "Valor");
+        while (in.hasNextLine()) {
+            String[] cont =in.nextLine().split(";");
+            System.out.printf("%-10s | %-10s | %-21s | %-15s | %-32s | %-10s%n", cont[0], cont[1], cont[2], cont[3], cont[4], cont[5]);
         }
     }
     public static int numVendas() throws FileNotFoundException {
@@ -110,7 +147,7 @@ public class FunctionsADM {
                 notFind=false;
                 System.out.println("******************** id "+idCliente+" ********************");
                 System.out.println("Nome: "+cont[1]+"\t||\tTelemovel: "+cont[2]+"\nEmail: "+cont[3]);
-                System.out.println("**********************************************");
+                System.out.println("***********************************************");
             }
         }
         if (notFind){
@@ -226,6 +263,100 @@ public class FunctionsADM {
         }
         System.out.println("A melhor categoria e: "+matrizCategorias[indexMelhor][0]);
     }
+    public static void pesquisaVendas(String jogo) throws FileNotFoundException {
+        Scanner inVendas = new Scanner(new File(pathMenuFiles("vendas")));
+        inVendas.nextLine();
+        System.out.println("************ Clientes Compraram ***************");
+        while (inVendas.hasNextLine()) {
+            String line = inVendas.nextLine();
+            String[] cont = line.split(";");
+            if(cont[4].equals(jogo)){
+                Scanner inClientes=new Scanner(new File(pathMenuFiles("clientes")));
+                inClientes.nextLine();
+                while (inClientes.hasNextLine()){
+                    String lineClientes=inClientes.nextLine();
+                    String[] contClientes=lineClientes.split(";");
+                    if(cont[1].equals(contClientes[0])){
+                        pesqCliente(contClientes[0]);
+                    }
+                }
+            }
+        }
+        System.out.println("***********************************************");
+    }
+    public static String[][] matrizJogosLucro() throws FileNotFoundException {
+        String[][] jogosLucro = new String[quantosJogosSemDup()][2];
+        Scanner in = new Scanner(new File(pathMenuFiles("vendas")));
+        in.nextLine();
+        String[] jogosSemDup=new String[quantosJogosSemDup()];
+        jogosSemDup=jogosSemDup();
+        int index=0;
+        for(String jogo: jogosSemDup){
+            jogosLucro[index][0]=jogo;
+            jogosLucro[index][1]="0";
+            index++;
+        }
+        String[][] matrizCategorias=new String[contaLines(pathMenuFiles("categorias"))][2];
+        Scanner inCategorias=new Scanner(new File(pathMenuFiles("categorias")));
+        inCategorias.nextLine();
+        int indexMatriz=0;
+        while (inCategorias.hasNextLine()){
+            String[] cont=inCategorias.nextLine().split(";");
+            matrizCategorias[indexMatriz][0]=cont[0];
+            matrizCategorias[indexMatriz][1]=cont[1];
+            indexMatriz++;
+        }
+        while (in.hasNextLine()){
+            String[] cont =in.nextLine().split(";");
+            for (int i = 0; i < jogosSemDup.length; i++) {
+                if(jogosLucro[i][0].equals(cont[4])){
+                    for (int j = 0; j < contaLines(pathMenuFiles("categorias")); j++) {
+                        if(matrizCategorias[j][0].equals(cont[3])){
+                            jogosLucro[i][1]=String.valueOf(Double.parseDouble(jogosLucro[i][1])+(Double.parseDouble(cont[5])*Double.parseDouble(matrizCategorias[j][1])/100));
+                        }
+                    }
+                }
+            }
+        }
+        return jogosLucro;
+    }
+    public static String[][] organizaMatriz(String[][] matriz){
+        boolean trocou;
+        for (int i = 0; i < matriz.length-1; i++) {
+            trocou=false;
+            for (int j = 0; j < matriz.length-1-i; j++) {
+                if(Double.parseDouble(matriz[j][1])>Double.parseDouble(matriz[j+1][1])){
+                    String auxNome=matriz[j][0];
+                    String auxValor=matriz[j][1];
+                    matriz[j][0]=matriz[j+1][0];
+                    matriz[j+1][0]=auxNome;
+                    matriz[j][1]=matriz[j+1][1];
+                    matriz[j+1][1]=auxValor;
+                    trocou=true;
+                }
+            }
+            if (!trocou) {
+                break;
+            }
+        }
+        return matriz;
+    }
+    public static void top5Jogos(String[][] matrizOrdenada){
+        System.out.println("********** TOP 5 Jogos **********");
+        System.out.printf("%-4s   %-25s   %-10s%n", "Nº", "Jogo", "Lucrou");
+        int lugar=1;
+        for (int i = matrizOrdenada.length-1; i > matrizOrdenada.length-6 ; i--) {
+            System.out.printf("%-4s | %-25s | %-10s%n",lugar,matrizOrdenada[i][0],matrizOrdenada[i][1]);
+            lugar++;
+        }
+    }
+    public static void  bot5Jogos(String[][] matrizOrdenada){
+        System.out.println("********** BOTTOM 5 Jogos **********");
+        System.out.printf("%-4s   %-30s   %-10s%n", "Nº", "Jogo", "Lucrou");
+        for (int i = 0; i < 5 ; i++) {
+            System.out.printf("%-4s | %-30s | %-10s%n",i+1,matrizOrdenada[i][0],matrizOrdenada[i][1]);
+        }
+    }
     public static void main(String[] args) throws FileNotFoundException {
         //userLogin("it","rojao");
         //printFile();
@@ -235,7 +366,12 @@ public class FunctionsADM {
         //System.out.println(jogoCaro()+"\t||\t"+valorJogoCaro());
         //imprimeJogoComprado(jogoCaro());
         //System.out.println("O id do melhor cliente e: "+melhorCliente());
-        //pesqCliente(melhorCliente());
-        melhorCategoria();
+        pesqCliente(melhorCliente());
+        //melhorCategoria();
+        //String[][] jogosLucroMatriz=matrizJogosLucro();
+        String[][] jogosOrdenados=organizaMatriz(matrizJogosLucro());
+        top5Jogos(jogosOrdenados);
+        bot5Jogos(jogosOrdenados);
+        pesquisaVendas("Sims 4 Digital Deluxe");
     }
 }
--- java/GameStart-TrabalhoFinal/src/GameStart/FunctionsClient.java
+++ java/GameStart-TrabalhoFinal/src/GameStart/FunctionsClient.java
@@ -1,4 +1,110 @@
 package GameStart;
 
+import com.sun.source.tree.WhileLoopTree;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static GameStart.FunctionsADM.contaLines;
+import static GameStart.PathFile.*;
+
 public class FunctionsClient {
+    public static void addCliente(String nome,String telemovel,String email) throws IOException {
+        int id=contaLines(pathMenuFiles("clientes"))+1;
+        String line=id+";"+nome+";"+telemovel+";"+email;
+        FileWriter writer =new FileWriter(new File(pathMenuFiles("clientes")),true);
+        writer.write("\n"+line);
+        System.out.println("Cliente adicionado.");
+        writer.close();
+    }
+    public static int quantosJogosSemDup() throws FileNotFoundException {
+        Scanner in=new Scanner(new File(pathMenuFiles("vendas")));
+        in.nextLine();
+        int contador=0;
+        while(in.hasNextLine()){
+            boolean naoTem=true;
+            String[] cont=in.nextLine().split(";");
+            Scanner inDentro=new Scanner(new File(pathMenuFiles("vendas")));
+            inDentro.nextLine();
+            while (inDentro.hasNextLine()){
+                String[] contDentro=inDentro.nextLine().split(";");
+                if(contDentro[0].equals(cont[0])){
+                    break;
+                }
+            }
+            while (inDentro.hasNextLine()){
+                String[] contDentro=inDentro.nextLine().split(";");
+                if(cont[4].equals(contDentro[4])){
+                    naoTem=false;
+                }
+            }
+            inDentro.close();
+            if(naoTem){
+                contador++;
+            }
+        }
+        in.close();
+        return contador;
+    }
+    public static String[] jogosSemDup() throws FileNotFoundException {
+        Scanner in=new Scanner(new File(pathMenuFiles("vendas")));
+        in.nextLine();
+        String[] jogosSemDup=new String[quantosJogosSemDup()];
+        int index=0;
+        while(in.hasNextLine()){
+            boolean naoTem=true;
+            String[] cont=in.nextLine().split(";");
+            Scanner inDentro=new Scanner(new File(pathMenuFiles("vendas")));
+            inDentro.nextLine();
+            while (inDentro.hasNextLine()){
+                String[] contDentro=inDentro.nextLine().split(";");
+                if(contDentro[0].equals(cont[0])){
+                    break;
+                }
+            }
+            while (inDentro.hasNextLine()){
+                String[] contDentro=inDentro.nextLine().split(";");
+                if(cont[4].equals(contDentro[4])){
+                    naoTem=false;
+                }
+            }
+            inDentro.close();
+            if(naoTem){
+                jogosSemDup[index]=cont[4];
+                index++;
+            }
+        }
+        in.close();
+        return jogosSemDup;
+    }
+    public static boolean triangular(int num){
+        int soma=0;
+        for (int i = 1; i <= num; i++) {
+            soma+=i;
+            if(soma>=num){
+                i=num;
+            }
+        }
+        return soma == num;
+    }
+    public static void lugarVago(){
+        for (int i = 1; i <= 121; i++) {
+            if(triangular(i)&&i%5==0){
+                System.out.println("Tem uma vaga no lugar: "+i);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        /*System.out.println("Tem "+quantosJogosSemDup()+" jogos.");
+        String[] jogosSemDup=new String[quantosJogosSemDup()];
+        jogosSemDup=jogosSemDup();
+        for(String jogo : jogosSemDup){
+            System.out.println(jogo);
+        }*/
+        //addCliente("Duarte","99","abd");
+        lugarVago();
+    }
 }
